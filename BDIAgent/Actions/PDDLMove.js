6df40ade5a978e5563e470c80720c43355822567
Davide <demartini.davide01@gmail.com>
2024-07-29 16:11:10 +0200

    first try with pddl

--- BDIAgent/Actions/PDDLMove.js
+++ BDIAgent/Actions/PDDLMove.js
@@ -2,6 +2,7 @@ import Plan from "../Plan.js";
 import client from "../client.js";
 import { astar, Graph } from '../astar.js';
 import { onlineSolver, PddlExecutor, PddlProblem, Beliefset } from "@unitn-asa/pddl-client";
+import { PDDLPlanner } from "../PDDL/pddlPlanner.js";
 
 
 class PDDLMove extends Plan {
@@ -14,11 +15,46 @@ class PDDLMove extends Plan {
         const graph = new Graph(grid.getMap());
         const start = graph.grid[Math.floor(me.x)][Math.floor(me.y)];
         const end = graph.grid[x][y];
-        const result = astar.search(graph, start, end);
 
-        beliefSet = new Beliefset();
-        beliefSet.declare('')
+        const planner = new PDDLPlanner(grid, start, end);
+        const problem = await planner.getProblem();
+        const domain = await fs.readFileSync('./BDIAgent/PDDL/domain.pddl', 'utf8').replace(/\r?\n|\r/g, '').replace(/\s\s+/g, ' ');
 
+        console.log("Sending to remote planner");
+
+        const plan = await onlineSolver(domain, problem);
+
+        if ( plan ) {
+            for (let i = 0; i < plan.length; i++) {
+
+                let action = plan[i];
+                let status = undefined;
+                // TODO magari cambiare nome azioni nel dominio pddl così da non dover fare questo switch
+                if (action.action == 'move_up') {
+                    status = await client.move('up');
+                }
+
+                if (action.action == 'move_down') {
+                    status = await client.move('down');
+                }
+
+                if (action.action == 'move_left') {
+                    status = await client.move('left');
+                }
+
+                if (action.action == 'move_right') {
+                    status = await client.move('right');
+                }
+
+                if (status) {
+                    me.x = status.x;
+                    me.y = status.y;
+                }
+                else {
+                    throw ['stucked'];
+                }
+            }
+        }
 
     }
 
