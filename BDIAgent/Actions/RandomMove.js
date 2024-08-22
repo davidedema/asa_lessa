89011c04d8fdded70cbf979b6f25757705603452
MattiaRigon <rigonmattia@gmail.com>
2024-08-22 08:56:54 +0200

    fix random move for split_map strategy

--- BDIAgent/Actions/RandomMove.js
+++ BDIAgent/Actions/RandomMove.js
@@ -7,7 +7,16 @@ class RandomMove extends Plan {
     }
 
     async execute(intentionRevision,father_desire,grid, me) {
-        const destination = grid.getSpawnPoints();
+        let destination = grid.getSpawnPoints();
+        if(me.strategy === "split_map"){
+            destination = destination.filter(point => {
+                if (point.x >= me.splitMapZone.minX && point.x <= me.splitMapZone.maxX && point.y >= me.splitMapZone.minY && point.y <= me.splitMapZone.maxY) {
+                    return true;
+                }else{
+                    return false;
+                }
+            });
+        }
         const randomIndex = Math.floor(Math.random() * destination.length);
         const { x, y } = destination[randomIndex];
         await this.subIntention(intentionRevision, "random_move", 'go_to_astar', { x, y }, grid, me);
